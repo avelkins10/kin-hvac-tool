989c4278d9b56047735c06f71406725fc813754e
Austin Elkins <you@example.com>
2026-01-23 20:46:06 -0700

    Fix builder page: move dynamic import to client component

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,9 +1,7 @@
 import { requireAuth } from '@/lib/auth-helpers'
-import dynamic from 'next/dynamic'
-
-const App = dynamic(() => import('@/src/App'), { ssr: false })
+import { BuilderApp } from '@/components/builder/BuilderApp'
 
 export default async function BuilderPage() {
   await requireAuth()
-  return <App />
+  return <BuilderApp />
 }
